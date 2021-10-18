727d5999fafa579fbc3c604ecc0aa11149c325fa
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2021-10-18 23:33:48 +0530

    refactor: Provide AppNavigator Interface using "Binds" annotated abstract function inside Abstract Hilt Module scoped to Activity container

--- app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
+++ app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
@@ -16,28 +16,31 @@
 
 package com.example.android.hilt.ui
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.android.hilt.LogApplication
 import com.example.android.hilt.data.LoggerLocalDataSource
 import com.example.android.hilt.databinding.FragmentButtonsBinding
 import com.example.android.hilt.navigator.AppNavigator
 import com.example.android.hilt.navigator.Screens
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 /**
  * Fragment that displays buttons whose interactions are recorded.
  */
+@AndroidEntryPoint
 class ButtonsFragment : Fragment() {
 
     private var _binding: FragmentButtonsBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var logger: LoggerLocalDataSource
-    private lateinit var navigator: AppNavigator
+    @Inject
+    lateinit var logger: LoggerLocalDataSource
+    @Inject
+    lateinit var navigator: AppNavigator
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,20 +50,6 @@ class ButtonsFragment : Fragment() {
         FragmentButtonsBinding.inflate(inflater, container, false)
             .apply { _binding = this }
             .root
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-
-        populateFields(context)
-    }
-
-    private fun populateFields(context: Context) {
-        logger = (context.applicationContext as LogApplication).serviceLocator.loggerLocalDataSource
-
-        navigator = (context.applicationContext as LogApplication).serviceLocator.provideNavigator(
-            requireActivity()
-        )
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.button1.setOnClickListener {
